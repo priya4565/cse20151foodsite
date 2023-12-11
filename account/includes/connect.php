7c822b7cadb80c7ecbcbcd792c8db91fbe3e5047
priya4565 <93039352+priya4565@users.noreply.github.com>
2023-12-11 16:12:04 +0530

    connect.php - removed name role routers.php - added above

--- account/includes/connect.php
+++ account/includes/connect.php
@@ -4,7 +4,12 @@ $servername = "cse20151db.mysql.database.azure.com";
 $server_user = "cse20151root";
 $server_pass = "abc_1234";
 $dbname = "cse20151db";
-$name = $_SESSION['name'];
-$role = $_SESSION['role'];
+
+// Create connection
 $con = new mysqli($servername, $server_user, $server_pass, $dbname);
+
+// Check connection
+if ($con->connect_error) {
+    die("Connection failed: " . $con->connect_error);
+}
 ?>
--- account/routers/router.php
+++ account/routers/router.php
@@ -1,50 +1,44 @@
 <?php
-include '../includes/connect.php';
-$success=false;
+session_start();
+
+$servername = "cse20151db.mysql.database.azure.com";
+$server_user = "cse20151root";
+$server_pass = "abc_1234";
+$dbname = "cse20151db";
+
+$con = new mysqli($servername, $server_user, $server_pass, $dbname);
+
+$success = false;
 
 $username = $_POST['username'];
 $password = $_POST['password'];
 
 $result = mysqli_query($con, "SELECT * FROM users WHERE username='$username' AND password='$password' AND role='Administrator' AND not deleted;");
-while($row = mysqli_fetch_array($result))
-{
-	$success = true;
-	$user_id = $row['id'];
-	$name = $row['name'];
-	$role= $row['role'];
+while ($row = mysqli_fetch_array($result)) {
+    $success = true;
+    $user_id = $row['id'];
+    $_SESSION['name'] = $row['name']; // Set session variables directly here
+    $_SESSION['role'] = $row['role']; // Set session variables directly here
 }
-if($success == true)
-{	
-	session_start();
-	$_SESSION['admin_sid']=session_id();
-	$_SESSION['user_id'] = $user_id;
-	$_SESSION['role'] = $role;
-	$_SESSION['name'] = $name;
 
-	header("location: ../admin-page.php");
-}
-else
-{
-	$result = mysqli_query($con, "SELECT * FROM users WHERE username='$username' AND password='$password' AND role='Customer' AND not deleted;");
-	while($row = mysqli_fetch_array($result))
-	{
-	$success = true;
-	$user_id = $row['id'];
-	$name = $row['name'];
-	$role= $row['role'];
-	}
-	if($success == true)
-	{
-		session_start();
-		$_SESSION['customer_sid']=session_id();
-		$_SESSION['user_id'] = $user_id;
-		$_SESSION['role'] = $role;
-		$_SESSION['name'] = $name;			
-		header("location: ../index.php");
-	}
-	else
-	{
-		header("location: ../login.php");
-	}
+if ($success == true) {
+    $_SESSION['admin_sid'] = session_id();
+    $_SESSION['user_id'] = $user_id;
+    header("location: ../admin-page.php");
+} else {
+    $result = mysqli_query($con, "SELECT * FROM users WHERE username='$username' AND password='$password' AND role='Customer' AND not deleted;");
+    while ($row = mysqli_fetch_array($result)) {
+        $success = true;
+        $user_id = $row['id'];
+        $_SESSION['name'] = $row['name']; // Set session variables directly here
+        $_SESSION['role'] = $row['role']; // Set session variables directly here
+    }
+    if ($success == true) {
+        $_SESSION['customer_sid'] = session_id();
+        $_SESSION['user_id'] = $user_id;
+        header("location: ../index.php");
+    } else {
+        header("location: ../login.php");
+    }
 }
 ?>
