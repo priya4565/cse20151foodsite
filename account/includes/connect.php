5e92c50e86973ee9d07a3f8cc416720cf13e1d57
priya4565 <93039352+priya4565@users.noreply.github.com>
2023-12-11 18:47:12 +0530

    updated name and role to connect.php

--- account/includes/connect.php
+++ account/includes/connect.php
@@ -4,6 +4,8 @@ $servername = "cse20151db.mysql.database.azure.com";
 $server_user = "cse20151root";
 $server_pass = "abc_1234";
 $dbname = "cse20151db";
+$name = $_SESSION['name'];
+$role = $_SESSION['role'];
 
 // Create connection
 $con = new mysqli($servername, $server_user, $server_pass, $dbname);
