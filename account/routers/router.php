7670e7a97059cdbebe19c007c7e19dd772010765
priya4565 <93039352+priya4565@users.noreply.github.com>
2023-12-12 00:42:54 +0530

    incorporated azure function to router.php

--- account/routers/router.php
+++ account/routers/router.php
@@ -8,37 +8,63 @@ $dbname = "cse20151db";
 
 $con = new mysqli($servername, $server_user, $server_pass, $dbname);
 
-$success = false;
-
 $username = $_POST['username'];
 $password = $_POST['password'];
 
-$result = mysqli_query($con, "SELECT * FROM users WHERE username='$username' AND password='$password' AND role='Administrator' AND not deleted;");
-while ($row = mysqli_fetch_array($result)) {
-    $success = true;
-    $user_id = $row['id'];
-    $_SESSION['name'] = $row['name']; // Set session variables directly here
-    $_SESSION['role'] = $row['role']; // Set session variables directly here
-}
+// Azure Function URL
+$azureFunctionUrl = 'https://cse20151fn.azurewebsites.net/api/HttpTrigger1?code=edTGGwacUUCbyt7ENuKvHq3LzW9jgq1lWavGiW63J2gPAzFumQFKpQ=='; // Replace with your actual Azure Function URL
 
-if ($success == true) {
-    $_SESSION['admin_sid'] = session_id();
-    $_SESSION['user_id'] = $user_id;
-    header("location: ../admin-page.php");
+// Data to send to Azure Function
+$data = [
+    'action' => 'login',
+    'username' => $username,
+    'password' => $password
+];
+
+// Initialize cURL session
+$ch = curl_init($azureFunctionUrl);
+
+// Set cURL options
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_POST, true);
+curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($data));
+curl_setopt($ch, CURLOPT_HTTPHEADER, [
+    'Content-Type: application/json',
+]);
+
+// Execute cURL session and get the response
+$response = curl_exec($ch);
+
+// Check for cURL errors
+if ($response === false) {
+    $error = curl_error($ch);
+    echo 'cURL Error: ' . $error;
 } else {
-    $result = mysqli_query($con, "SELECT * FROM users WHERE username='$username' AND password='$password' AND role='Customer' AND not deleted;");
-    while ($row = mysqli_fetch_array($result)) {
-        $success = true;
-        $user_id = $row['id'];
-        $_SESSION['name'] = $row['name']; // Set session variables directly here
-        $_SESSION['role'] = $row['role']; // Set session variables directly here
-    }
-    if ($success == true) {
-        $_SESSION['customer_sid'] = session_id();
-        $_SESSION['user_id'] = $user_id;
-        header("location: ../index.php");
+    // Handle the response from Azure Function
+    $responseData = json_decode($response, true);
+    if ($responseData && isset($responseData['status']) && $responseData['status'] === 200) {
+        // Login successful, handle accordingly
+        $userData = json_decode($responseData['body'], true);
+        $role = $userData['role']; // Assuming role is present in the Azure Function response
+        $_SESSION['name'] = $userData['username']; // Set session variables
+        $_SESSION['role'] = $role; // Set session variables
+
+        if ($role === 'Administrator') {
+            $_SESSION['admin_sid'] = session_id();
+            header("location: ../admin-page.php");
+        } else if ($role === 'Customer') {
+            $_SESSION['customer_sid'] = session_id();
+            header("location: ../index.php");
+        } else {
+            // Handle other roles or scenarios as needed
+        }
     } else {
+        // Login failed, handle accordingly
+        echo 'Login failed: ' . $responseData['body'];
         header("location: ../login.php");
     }
 }
+
+// Close cURL session
+curl_close($ch);
 ?>
